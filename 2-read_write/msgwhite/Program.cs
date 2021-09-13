c9c8793511d6b55f910a84c35a02c70a13124418
AGG <a20100306@live.ceti.mx>
2021-09-13 17:17:52 -0500

    VSC

---
+++ 2-read_write/msgwhite/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace msgwrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" \n Hola ");
+            Console.Write("como ");
+            Console.Write("estas ");
+            Console.Write(">_<. \n ");
+        }
+    }
+}
---
+++ 2-read_write/msgwhiteline/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace msgwhiteline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string empresa = "AGG";
+            string elaboracion = "DESING";
+            string servicio = "WEB"; 
+
+            Console.WriteLine("\n   Empieza tu nueva WEB");
+            Console.WriteLine("\n   Con " + empresa + " " + elaboracion + " la carga no sera todo tuya. ");
+            Console.WriteLine("\n   Interpolacion de Strings ");//la interpolacion es la interjeccion de strings en texto por medio de "{}" y "$".
+            Console.WriteLine($"\n   Ahora {empresa} {elaboracion} {servicio} sabes lo que te brinda\n");
+
+        }
+    }
+}
