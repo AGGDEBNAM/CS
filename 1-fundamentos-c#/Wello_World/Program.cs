11ad44a3a48612e8090e95de51d995acb8aa94eb
AGGDEBNAM <a20100306@live.ceti.mx>
2021-09-12 22:24:40 -0500

    VSC

---
+++ 1-fundamentos-c#/Wello_World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
