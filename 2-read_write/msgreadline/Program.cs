c20f0e5c41b86cb81b6c0741b65232dd417ed695
AGG <a20100306@live.ceti.mx>
2021-09-13 21:02:39 -0500

    SVC

--- 2-read_write/msgreadline/Program.cs
+++ 2-read_write/msgreadline/Program.cs
@@ -10,6 +10,8 @@ namespace msgreadline
             Console.WriteLine("\nHola me gustaria saber tu nombre:\n");
             Console.WriteLine("\nEs un bonito nombre " + $"{Console.ReadLine()}\n");
             //Un readline solo se puede ejecutar dentro de la funcion writeline colocado de la forma que se ve.
+            //string nombre = Console.ReadLine();
+            //La unica forma alternativa es añadiendo de esta forma en el espacio solicitado.
 
         }
     }
---
+++ 2-read_write/practica_msg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace practica_msg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n====DATOS PARA ENCUESTADO====\n");
+            Console.WriteLine(" Cual es tu Nombre:\n");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("\n Apellidos:\n");
+            string apellidos = Console.ReadLine();
+            Console.WriteLine("\n Cual es tu Edad:\n");
+            string edad = Console.ReadLine();
+            Console.WriteLine("\n Que es lo que te Apaciona:\n");
+            string pasion = Console.ReadLine();
+            Console.WriteLine("====RESULTADOS DEL ENCUESTADO====\n");
+            Console.WriteLine($"Se llama: \n{nombre} {apellidos}");
+            Console.WriteLine($"\nSu edad es: \n{edad} años");
+            Console.WriteLine($"\nSu pasion es: \n{pasion}\n");
+
+        }
+    }
+}
