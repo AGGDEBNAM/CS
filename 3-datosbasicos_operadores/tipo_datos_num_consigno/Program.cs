074d6d56f1b22c1d39f9d5e0586d050f5eb842de
AGG <a20100306@live.ceti.mx>
2021-09-13 23:39:06 -0500

    VSC

--- 3-datosbasicos_operadores/tipo_datos_num_consigno/Program.cs
+++ 3-datosbasicos_operadores/tipo_datos_num_consigno/Program.cs
@@ -6,7 +6,19 @@ namespace tipo_datos_num_consigno
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tipos de datos numericos - Enteros - Consigno");
+
+            sbyte sbyteValorValido = 127;
+            short shortValorValido = 32767;
+            int intValorValido = 2147483647;
+            long longValorValido = 9223372036854775807;
+            //Todos los datos son valores enteros con signos al igual que se ven
+            //en la variable su valor negativo es el mismo -1.Ejemplo_127=-127-1=-128 
+            Console.WriteLine(sbyteValorValido);
+            Console.WriteLine(shortValorValido);
+            Console.WriteLine(intValorValido);
+            Console.WriteLine(longValorValido);
+
         }
     }
 }
