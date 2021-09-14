6c9cae4090ab104c9d773dc6980e5959f48bf0e6
AGG <a20100306@live.ceti.mx>
2021-09-13 23:08:33 -0500

    VSC

---
+++ 3-datosbasicos_operadores/tipo_datos_num_consigno/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tipo_datos_num_consigno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
