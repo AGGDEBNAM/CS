f88a946ad2e8f1b893f59e351dae8186bbb6bfc6
AGG <a20100306@live.ceti.mx>
2021-09-14 09:32:55 -0500

    VSC

---
+++ 3-datosbasicos_operadores/tipo_datos_num_sinsigno/tipo_datos_num_sinsigno/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace tipo_datos_num_sinsigno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Tipos de datos numericos - Enteros - Sinsigno");
+
+            byte byteValorValido = 255;
+            ushort ushortValorValido = 65535;
+            uint uintValorValido = 4294967295;
+            ulong ulongValorValido = 18446744073709551615;
+            //Todos los datos son valores enteros que inician [0,valor expresado en varibles]
+            //por lo que es lo doble que una consigno.
+            //Lo unico que cambia es la iniciacion de estos con una u y en caso de sbyte es byte.
+            Console.WriteLine(byteValorValido);
+            Console.WriteLine(ushortValorValido);
+            Console.WriteLine(uintValorValido);
+            Console.WriteLine(ulongValorValido);
+
+        }
+    }
+}
