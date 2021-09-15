3e12e270bd332f65c9354cd3d43ddb00eb793732
AGG <a20100306@live.ceti.mx>
2021-09-14 23:14:49 -0500

    VSC

--- 2-read_write/msgreadline/Program.cs
+++ 2-read_write/msgreadline/Program.cs
@@ -9,9 +9,9 @@ namespace msgreadline
             
             Console.WriteLine("\nHola me gustaria saber tu nombre:\n");
             Console.WriteLine("\nEs un bonito nombre " + $"{Console.ReadLine()}\n");
-            //Un readline solo se puede ejecutar dentro de la funcion writeline colocado de la forma que se ve.
-            //string nombre = Console.ReadLine();
-            //La unica forma alternativa es añadiendo de esta forma en el espacio solicitado.
+            //Se puede poner un readline dentro del write line que va aadir un mensaje al texto entrante.
+            //string 'nombre' = Console.ReadLine(); <--- La unica forma alternativa es añadiendo de esta forma en el espacio solicitado.
+
 
         }
     }
