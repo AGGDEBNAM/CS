b75e0f209a119928d53e9dbec55bf951605ee657
AGG <a20100306@live.ceti.mx>
2021-09-16 15:31:25 -0500

    VSC

---
+++ 3-datosbasicos_operadores/operadores_aritmeticos_binarios_suma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace operadores_aritmeticos_binarios_suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\nOperacionesAritmeticas-Binaria-Suma");
+
+            Console.Write("\nIntroduce tu primera cifra: ");
+            string string_0 = Console.ReadLine();
+            Console.Write("\nIntroduce tu primera cifra: ");
+            string string_1 = Console.ReadLine();
+
+            double suma_0 = Convert.ToDouble(string_0);
+            double suma_1 = Convert.ToDouble(string_1);
+
+            Console.WriteLine($"\nLa suma de {string_0} + {string_1} = {suma_0 + suma_1}");
+
+        }
+    }
+}
