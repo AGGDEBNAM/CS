b9f9547438acfba0469c12cb969d99b93c4e2190
AGG <a20100306@live.ceti.mx>
2021-09-15 22:51:15 -0500

    VSC

---
+++ 3-datosbasicos_operadores/tipo_conversiones_clasesconvert/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace tipo_conversiones_clasesconvert
+{
+    //para saber mas detalles se ofrece la documentacion de onenote '3-datosbasicos_operadores' para saber todos los tipos de convert.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n   ALGUNOS TIPOS DE TO.CONVERT   \n");
+
+            int myInt = 98;
+            double myDouble = 13.69;
+            bool myBool = true;
+            char myChar = '#';
+            //sencillamente convierte valores de un tipo de variable a una en la que se pueda convertir pudiendo decir que es una forma automatica de detectar implicito y explicito.
+
+            Console.WriteLine($"Mi int '98' a Char es: {Convert.ToChar(myInt)}");
+            Console.WriteLine($"Mi double '13.69' a String es: {Convert.ToString(myDouble)}");
+            Console.WriteLine($"Mi bool 'true' a Int es: {Convert.ToInt32(myBool)}"); 
+            Console.WriteLine($"Mi char '#' a Byte es: {Convert.ToSByte(myChar)}");
+
+            Console.WriteLine("\n   ALGUNOS TIPOS DE TO.CONVERT - SALIENDO DE CONVERSION EXPLICITA   \n");
+            //Si combinamos ambas funciones podemos convertir un valor ya convertido.
+
+            double myDouble_0 = 270.7;
+            long myLong_0 = (long) myDouble_0;
+            char myChar_0 = (char) myDouble_0;
+            
+            Console.WriteLine($"Mi double '270.7' a int es: {Convert.ToInt32(myDouble_0)}");
+            Console.WriteLine($"Mi long '270' a bool es: {Convert.ToBoolean(myLong_0)}");
+            Console.WriteLine($"Mi char 'D' a String es: {Convert.ToString(myChar_0)}");
+            
+            Console.WriteLine("\n   ALGUNOS TIPOS DE TO.CONVERT - SALIENDO DE CONVERSION IMPLICITA   \n");
+            //Si combinamos ambas funciones podemos convertir un valor ya convertido.
+
+            char myChar_1 = 'W';
+            long myLong_1 = myChar_1;
+            double myDouble_1 = myLong_1;
+
+            Console.WriteLine($"Mi char 'W' a int es: {Convert.ToInt32(myChar_1)}");
+            Console.WriteLine($"Mi long '87' a bool es: {Convert.ToBoolean(myLong_1)}");
+            Console.WriteLine($"Mi Double '87' a String es: {Convert.ToString(myDouble_1)}");
+
+            Console.WriteLine("\n   ALGUNOS TIPOS DE TO.CONVERT - SIN VALORES DECLARADOS   \n");
+            //Esta funcion sin declarar variables se debe a que se declaran el en espacio donde deveria y la variable.
+
+            Console.WriteLine($"Convert.ToInt32('#') = {Convert.ToInt32('#')}");
+            Console.WriteLine($"Convert.ToString(true) = {Convert.ToString(true)}");
+            Console.WriteLine($"Convert.ToChar('u0092') = {Convert.ToChar('\u0092')}");
+            Console.WriteLine($"Convert.ToBoolean(0) = {Convert.ToBoolean(0)}");
+            Console.WriteLine($"Convert.ToChar(34) = {Convert.ToChar(34)}");
+            Console.WriteLine($"Convert.ToDouble(44.54) = {Convert.ToDouble(44.54)}");
+
+
+        }
+    }
+}
---
+++ 3-datosbasicos_operadores/tipo_conversiones_explicitas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace tipo_conversiones_explicitas
+{
+    class Program
+    {
+        //double -> float -> long -> int -> char
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n========Conversiones-Explicitas========\n");
+            //Esta es una buena forma de hacer convesiones explicitas de forma que tus varibles ya estan asignadas a un valor inicial asignado.
+            Console.WriteLine("\nForma numero uno con varias varibles\n");
+
+            Double myDouble_0 = 69.69d;
+            float myFloat = (float) myDouble_0;
+            long myLong = (long) myFloat;
+            int myInt = (int) myLong;
+            char myChar = (char) myInt;
+            //es importante saber que puedes hacer saltos de tipos de varibles ejemplo de double a int pero nunca alreves como se muestra.
+
+            Console.WriteLine($"Mi double es {myDouble_0}");
+            Console.WriteLine($"Mi double a float es {myFloat}");
+            Console.WriteLine($"Mi float a long es {myLong}");
+            Console.WriteLine($"Mi long es int es {myInt}");
+            Console.WriteLine($"Mi int a char es {myChar}");
+            //hay dos formas de convertir que es declarando una variable con uno o varios textos a leer como en el ejemplo de abajo.
+            //la otra forma es con varias variables ya convertidas con uno o varios textos o uno a leer.
+            Console.WriteLine("\nForma numero dos con una varible\n");
+
+            Double myDouble_1 = 69.69d;
+
+            Console.WriteLine($"Mi valor double es de: {myDouble_1}");
+            Console.WriteLine($"Mi valor double a Float es de: {(float) myDouble_1}");
+            Console.WriteLine($"Mi valor double a Long es de: {(long) myDouble_1}");
+            Console.WriteLine($"Mi valor double a Int es de: {(int) myDouble_1}");
+            Console.WriteLine($"Mi valor double a Char es de: {(char) myDouble_1}");
+            
+        }
+    }
+}
