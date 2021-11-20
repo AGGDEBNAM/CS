fb615335bbc1e18a3401e84da90d409524c9fda2
AGGDEBNAM <a20100306@ceti.mx>
2021-11-20 17:54:37 -0600

    VSC

--- 3-datosbasicos_operadores/operadores_aritmeticos_binarios_resta_multiplicion/Program.cs
+++ 3-datosbasicos_operadores/operadores_aritmeticos_binarios_resta_multiplicion/Program.cs
@@ -7,7 +7,7 @@ namespace operadores_aritmeticos_binarios_suma
         static void Main(string[] args)
         {
 
-            Console.WriteLine("\nOperacionesAritmeticas-Binaria-Suma");
+            Console.WriteLine("\nOperacionesAritmeticas-Binaria-Resta_Multiplicacion");
 
             Console.Write("\nIntroduce tu primera cifra: ");
             string string_0 = Console.ReadLine();
@@ -17,11 +17,8 @@ namespace operadores_aritmeticos_binarios_suma
             double suma_0 = Convert.ToDouble(string_0);
             double suma_1 = Convert.ToDouble(string_1);
             
-            
-            Console.WriteLine($"\nLa suma de {string_0} + {string_1} = {suma_0 + suma_1}");
-            //^ El uso de estancias mas el el operador de suma hace que estas estancias que son las variables puedan
-            //^ ser cambiadas por el signo de menos ejemplo (-5) + (5) = 0 siguiendo la ley de signos.
+            Console.WriteLine($"\nLa suma de {string_0} * {string_1} = {suma_0 * suma_1}");
             
         }
     }
-} 
+}
